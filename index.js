dc6c9f1791407ae73423d0e82c1b0ade558e7e68
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-10-20 20:57:23 -0400

    might work?

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 "use strict";
 
-module.exports = function(){
+var request = require('superagent');
 
+module.exports.make = function(host, cb){
+    request.post("http://" + host + "/").end(function(res){
+        cb(Number(res.body));
+    });
+};
+
+module.exports.getLast = function(host, cb){
+    request.get("http://" + host + "/").end(function(res){
+        cb(Number(res.body));
+    });
 };
