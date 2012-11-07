73397f52b5b7804c825f91501bf50c1a8615b6bd
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-11-07 11:51:44 -0500

    My name is mud.

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var request = require('superagent');
 
 module.exports.make = function(host, cb){
     request.post("http://" + host + "/").end(function(res){
-        cb(Number(res.body));
+        cb(Number(res.text));
     });
 };
 
 module.exports.getLast = function(host, cb){
     request.get("http://" + host + "/").end(function(res){
-        cb(Number(res.body));
+        cb(Number(res.text));
     });
 };
